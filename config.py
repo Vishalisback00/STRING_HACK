91c243f28f408774c1c9a05034e6efcef3399637
Vishalisback00 <franceserver0308@gmail.com>
2025-10-01 17:14:40 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "27079591"
-    API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    API_ID = "26493077"
+    API_HASH = "6586f0276c7748e54684719bdd247d90"
+    TOKEN = "8451532457:AAHEaV8KH62kpLUoFmnWXoHiMVfkPILIYhU"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
-    SUDOERS = filters.user(["8143754205"])
+    MONGO_URL = "mongodb+srv://aarubhakar302:effOLpfZ0awCjQxz@cluster0.byhbxty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    START_PIC = "https://files.catbox.moe/o9bo13.jpg"
+    SUDOERS = filters.user(["7996314470"])
